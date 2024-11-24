c618ecaa09e98ee2029f5058df9656942d832e63
Yasaswi Gorantla <yasaswigorantla@gmail.com>
2024-11-24 10:59:07 +0530

    Update URL.py

--- URL.py
+++ URL.py
@@ -2,17 +2,16 @@ import os
 import streamlit as st
 import pickle
 import time
-from langchain_openai import OpenAI
+from langchain_community.llms import OpenAI
 from langchain.chains import RetrievalQAWithSourcesChain
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.llms import OpenAI
 from langchain_community.document_loaders import UnstructuredURLLoader
 from langchain_community.embeddings import OpenAIEmbeddings
 from langchain_community.vectorstores import FAISS
 from dotenv import load_dotenv
 
 load_dotenv()  # Load environment variables from .env
-llm = OpenAI(openai_api_key="OPENAI_API_KEY")
+llm = OpenAI(OPENAI_API_KEY="OPENAI_API_KEY")
 st.title("URL FETCHER")
 st.sidebar.title("Study Article URLs")
 
