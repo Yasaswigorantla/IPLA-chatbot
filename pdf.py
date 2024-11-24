b9f02358b11b1b6fc4ec1a38be3766ad2ee1f43f
Yasaswi Gorantla <yasaswigorantla@gmail.com>
2024-11-24 11:26:40 +0530

    Update pdf.py

--- pdf.py
+++ pdf.py
@@ -1,11 +1,12 @@
 
 import streamlit as st
 from PyPDF2 import PdfReader
-from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain_community.embeddings import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_openai import OpenAI
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
-from langchain.llms import OpenAI
+from langchain_community.llms import OpenAI
 from dotenv import load_dotenv
 load_dotenv()
 
